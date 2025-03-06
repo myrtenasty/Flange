717b391706bd2299c926402588bb893ad130db00
Preliy <vikt.gaponenko@gmail.com>
2025-03-06 23:18:25 +0100

    fix: frame config tooltip

--- Runtime/Scripts/Kinematic/FrameConfig.cs
+++ Runtime/Scripts/Kinematic/FrameConfig.cs
@@ -3,7 +3,7 @@
 namespace Preliy.Flange
 {
     /// <summary>
-    /// Denavit–Hartenberg frame configuration
+    /// Frame configuration using the Denavit-Hartenberg parameters
     /// </summary>
     [System.Serializable]
     public record FrameConfig
@@ -24,14 +24,24 @@ namespace Preliy.Flange
         [Tooltip("Alpha [rad]")]
         [SerializeField]
         private float _alpha;
+        [Tooltip("A [m]")]
         [SerializeField]
         private float _a;
+        [Tooltip("D [m]")]
         [SerializeField]
         private float _d;
         [Tooltip("Theta [rad]")]
         [SerializeField]
         private float _theta;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrameConfig"/> class using the Denavit-Hartenberg parameters
+        /// </summary>
+        /// <param name="alpha">The link twist (α) [rad], representing the angle between the z-axes of consecutive frames.</param>
+        /// <param name="a">The link length (a) [m], which is the distance between the z-axes along the common normal.</param>
+        /// <param name="d">The link offset (d) [m], the distance along the previous z-axis to the common normal.</param>
+        /// <param name="theta">The joint angle (θ) [rad], the rotation about the previous z-axis to align the x-axes.</param>
+        /// <param name="name">An optional identifier for the frame configuration.</param>
         public FrameConfig(float alpha, float a, float d, float theta, string name = null)
         {
             _name = name;
